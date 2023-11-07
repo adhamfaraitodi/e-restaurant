5f0ce006dc68922b844ed29716d65d362bc6e910
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-11-07 08:22:45 +0700

    change migration file

--- database/migrations/2023_11_07_004947_admin_table.php
+++ database/migrations/2023_11_07_004947_admin_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('admin_table', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name', 20);
+            $table->string('phone',20);
+            $table->string('address', 80);
+            $table->string('position', 15);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::drop('admin_table');
     }
 };
