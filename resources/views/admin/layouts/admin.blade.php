3102c5752d3bfe37680619be93de6ab0b1d714a4
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-11-15 11:55:03 +0700

    add slicing blade template

---
+++ resources/views/admin/layouts/admin.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('body_class','nav-md')
+
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col menu_fixed">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+                    </div>
+
+                    <div class="clearfix"></div>
+                    <br />
+                    @include('admin.section.sidebar')
+                </div>
+            </div>
+    @include('admin.section.header')
+            <!-- page content -->
+            <div class="right_col" role="main">
+                <div class="">
+                    <div class="page-title">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+            <!-- /page content -->
+        </div>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
