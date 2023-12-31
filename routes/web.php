13e2dfa499e919ed63f60cb3e85a11a7424849d3
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-12-31 18:12:36 +0700

    fiks readibility for IDE

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PesanController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\PesananController;
+use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\KaryawanController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\Auth\LoginRegisterController;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,59 +23,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'App\Http\Controllers'], function()
-{
-    Route::group(['middleware' => ['guest']], function() {
-        /**
-         * Routes admin
-         */
-        Route::get('/admin/login', 'LoginController@show')->name('login.show');
-        Route::post('/admin/login', 'LoginController@login')->name('login.perform');
-        /**
-         * Routes Customer
-         */
-        Route::get('/meja/{meja}', 'PesanController@show')->name('pesan.show');
-        Route::post('/customer/setupSession','PesanController@setupSession')->name('pesan.setupSession');
-        Route::group(['middleware' => ['customerSessionCheck']],function(){
-            Route::get('/meja/{meja}/menu', 'PesanController@menu')->name('pesan.menu');
-            Route::get('/meja/{meja}/menu/{id}', 'PesanController@addMenuCart')->name('pesan.add');
-            Route::get('/meja/{meja}/keranjang', 'PesanController@cartshow')->name('pesan.cartshow');
-            Route::get('/meja/{meja}/keranjang/checkout', 'PesanController@checkOut')->name('pesan.checkout');
-            Route::delete('/meja/{meja}/keranjang/{id}', 'PesanController@cartdelete')->name('pesan.cartdelete');
-            Route::get('/meja/{meja}/flush', 'PesanController@flush')->name('pesan.flush');
-            Route::get('/meja/{meja}/bayar', 'PesanController@test')->name('pesan.test');
-        });
-
-        Route::get('/', 'AdminController@show')->name('admin.show');
-
+Route::group(['middleware' => ['guest']], function() {
+    /**
+     * Routes admin
+     */
+    Route::get('/admin/login', [LoginController::class, 'show'])->name('login.show');
+    Route::post('/admin/login', [LoginController::class, 'login'])->name('login.perform');
+    /**
+     * Routes Customer
+     */
+    Route::get('/meja/{meja}', [PesanController::class, 'show'])->name('pesan.show');
+    Route::post('/customer/setupSession',[PesanController::class, 'setupSession'])->name('pesan.setupSession');
+    Route::group(['middleware' => ['customerSessionCheck']],function(){
+        Route::get('/meja/{meja}/menu', [PesanController::class, 'menu'])->name('pesan.menu');
+        Route::get('/meja/{meja}/menu/{id}', [PesanController::class, 'addMenuCart'])->name('pesan.add');
+        Route::get('/meja/{meja}/keranjang', [PesanController::class, 'cartshow'])->name('pesan.cartshow');
+        Route::get('/meja/{meja}/keranjang/checkout', [PesanController::class, 'checkOut'])->name('pesan.checkout');
+        Route::delete('/meja/{meja}/keranjang/{id}', [PesanController::class, 'cartdelete'])->name('pesan.cartdelete');
+        Route::get('/meja/{meja}/flush', [PesanController::class, 'flush'])->name('pesan.flush');
+        Route::get('/meja/{meja}/bayar', [PesanController::class, 'test'])->name('pesan.test');
     });
 
-    Route::group(['middleware' => ['auth']], function() {
-        Route::get('/admin', 'AdminController@dashboard')->name('admin.dashboard');
-        Route::get('/admin/register','RegisterController@show')->name('register.show');
-        Route::post('/admin/register','RegisterController@register')->name('register.perform');
+    Route::get('/', [AdminController::class, 'show'])->name('admin.show');
 
-        Route::get('/admin/menu/kelolamenu', 'MenuController@show')->name('menu.show');
-        Route::get('/admin/menu/edit/{menu}', 'MenuController@edit')->name('menu.edit');
-        Route::post('/admin/menu/update/{menu}', 'MenuController@update')->name('menu.update');
-        Route::post('/admin/menu/delete/{menu}', 'MenuController@destroy')->name('menu.destroy');
-        Route::post('/admin/menu/restore/{menu}', 'MenuController@restore')->name('menu.restore');
-        Route::get('/admin/menu/tambah', 'MenuController@create')->name('menu.create');
-        Route::post('/admin/menu/tambah', 'MenuController@store')->name('menu.store');
+});
 
-        Route::get('/admin/pesanan/masuk', 'PesananController@show')->name('pesan.show');
-        Route::get('/admin/pesanan/masuk/{id}', 'PesananController@detailshow')->name('pesan.detailshow');
-        Route::post('/admin/pesanan/selesai/{id}', 'PesananController@pesananselesai')->name('pesan.pesananselesai');
-        Route::get('/admin/pesanan/selesai', 'PesananController@selesaishow')->name('pesanselesai.show');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/register',[RegisterController::class, 'show'])->name('register.show');
+    Route::post('/admin/register',[RegisterController::class, 'register'])->name('register.perform');
+    Route::get('/admin/menu/kelolamenu', [MenuController::class, 'show'])->name('menu.show');
+    Route::get('/admin/menu/{menu}/edit', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::patch('/admin/menu/{menu}/update', [MenuController::class, 'update'])->name('menu.update');
+    Route::post('/admin/menu/{menu}/delete', [MenuController::class, 'destroy'])->name('menu.destroy');
+    Route::get('/admin/menu/tambah', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('/admin/menu/tambah', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('/admin/pesanan/masuk', [PesananController::class, 'show'])->name('pesan.show');
+    Route::get('/admin/pesanan/masuk/{id}', [PesananController::class, 'detailshow'])->name('pesan.detailshow');
+    Route::post('/admin/pesanan/selesai/{id}', [PesananController::class, 'pesananselesai'])->name('pesan.pesananselesai');
+    Route::get('/admin/pesanan/selesai', [PesananController::class, 'selesaishow'])->name('pesanselesai.show');
+    Route::get('/admin/laporan', [LaporanController::class, 'show'])->name('laporan.show');
 
-        Route::get('/admin/laporan', 'LaporanController@show')->name('laporan.show');
+    Route::get('/admin/karyawan',[KaryawanController::class, 'show'])->name('karyawan.show');
+    Route::post('/admin/karyawan/{id}/delete',[KaryawanController::class, 'delKaryawan'])->name('karyawan.delete');
+    Route::post('/admin/karyawan/{id}/update',[KaryawanController::class, 'UpdateKaryawan'])->name('karyawan.update');
 
-        Route::get('/admin/karyawan','KaryawanController@show')->name('karyawan.show');
-        Route::post('/admin/karyawan/{id}/delete','KaryawanController@delKaryawan')->name('karyawan.delete');
-        Route::post('/admin/karyawan/{id}/update','KaryawanController@UpdateKaryawan')->name('karyawan.update');
-        Route::get('/admin/karyawan/{id}/edit','KaryawanController@editKaryawan')->name('karyawan.edit');
+    Route::get('/admin/karyawan/{id}/edit',[KaryawanController::class, 'editKaryawan'])->name('karyawan.edit');
 
-        Route::get('/admin/logout', 'LogoutController@perform')->name('logout.perform');
-    });
+
+    Route::get('/admin/logout', [LogoutController::class, 'perform'])->name('logout.perform');
 });
 
